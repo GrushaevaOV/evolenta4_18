c76cf6d0fe32a63363b1ea451ac81a9df50d1f08
Ольга Гришаева <120657892+GrushaevaOV@users.noreply.github.com>
2023-08-19 18:00:43 +0300

    4_18 started

---
+++ src/main/java/com/example/evolenta4_18/Evolenta418Application.java
@@ -0,0 +1,13 @@
+package com.example.evolenta4_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evolenta418Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evolenta418Application.class, args);
+	}
+
+}
